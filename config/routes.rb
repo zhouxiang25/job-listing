73b8e1cc44f293434b1b2a1af3d1bd5cbf15ecc0
周祥 <xingzhewujiangcz@gmail.com>
2017-04-28 17:11:10 +0800

    step 7 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-   resources :jobs
+
    root 'jobs#index'
    namespace :admin do
     resources :jobs do
@@ -11,4 +11,10 @@ Rails.application.routes.draw do
       end
    end
  end
+
+  resources :jobs do
+    resources :resumes
+  end
+
+  root 'jobs#index'
 end
