fbb48004637fda5445941832ace9682ec2c3bab1
周祥 <xingzhewujiangcz@gmail.com>
2017-05-03 16:46:11 +0800

    better welcome/index & admin/jobs/index

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,20 @@ Rails.application.routes.draw do
   devise_for :users
 
 
-   root 'jobs#index'
+   root 'welcome#index'
    namespace :admin do
     resources :jobs do
       member do
         post :publish
         post :hide
       end
+   resources :resumes
    end
  end
 
   resources :jobs do
-    resources :resumes
-  end
-
+  resources :resumes
+end
   root 'jobs#index'
+
 end
